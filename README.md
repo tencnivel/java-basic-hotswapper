c947d3d699636427c156de59bf86c223a3a68d4b
Vincent Laugier <vincent.laugier@gmail.com>
2019-01-03 19:47:59 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # java-basic-hotswapper
-A java hotswapper for changes in methods
+A java hotswapper to automatically redefine classes that have received in-body code changes.
+
+Motivation of the project: 
+Redeploying a project in a JEE servers is quite fast and it is not that bad to have to redeploy 
+a project when important changes (eg. adding/renaming a class, adding/renaming a method, 
+changing the signature of a method, adding/renaming an attribute ) are made to the code.
+
+The real loss of time and productivity is to have to redeploy a project whenever we do 
+a modification to the code of a method.
+
+JDKs (since which version?) are shipped with all we need to reload classes to take into those 
+in-body code changes.
+
+JRebel has gotten too expensive, hotswapagent doesn't work well if you use symlinks to
+ deploy your project, IDE's hotswapping has too much adherence to the IDE (the IDE may 
+not support it, the server needs to be ran from the IDE).
+
+
+## Prerequisite
+
+### Enable JPDA in yout server so that we can attach to it
+eg. in wildfly/kboss, find the following in standalone.conf:
+
+```
+# Sample JPDA settings for remote socket debugging
+JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=n"
+```
+
+### Add tools.jar to your local maven repository
+
+Install tools.jar from you jdk (in the lib folder) in your local maven dependencies
+
+```
+mvn install:install-file -Dfile=path-to-your-artifact-jar \
+                         -DgroupId=your.groupId \
+                         -DartifactId=your-artifactId \
+                         -Dversion=version \
+                         -Dpackaging=jar
+```
+
+NOTE: tools.jar can come from a more recent JDK than the JVM to which we are attaching.
+
+## Usage
+Execute the main file located in **WatchDir** with arguments of the path to the directories containing 
+the classes we want to automatically reload at changes.
+
+NOTES: 
+
+ * If using symlink, declare the source directory and not the symlink, if not WatchDir 
+will not see the changes to the class files.
+ * The program can be executed before the server is started because it attaches to the JVM
+at every change (and not at startup)
---
+++ src/main/java/org/tencnivel/hotswapper/JDIRedefiner.java
@@ -0,0 +1,205 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tencnivel.hotswapper;
+
+import com.sun.jdi.AbsentInformationException;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.sun.jdi.Bootstrap;
+import com.sun.jdi.ReferenceType;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.VirtualMachineManager;
+import com.sun.jdi.connect.AttachingConnector;
+import com.sun.jdi.connect.Connector.Argument;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import org.apache.poi.util.IOUtils;
+
+/**
+ * 
+ * Adapted from: 
+ * https://www.programcreek.com/java-api-examples/?code=HotswapProjects/HotswapAgent/HotswapAgent-master/plugin/hotswap-agent-proxy-plugin/src/test/java/org/hotswap/agent/plugin/proxy/JDIRedefiner.java#
+ */
+public class JDIRedefiner {
+
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        
+//        
+//        
+//        try {
+//            // TODO code application logic here
+//            JDIRedefiner jDIRedefiner = new JDIRedefiner(8787);
+//            Map<ReferenceType,byte[]> map = jDIRedefiner.createMapOfReferenceTypesToBeRedefined_TEST();
+//            jDIRedefiner.redefineClasses(map);
+//
+//            jDIRedefiner.close();
+//        } catch (IOException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//        
+//    }
+    
+    private Logger logger = Logger.getLogger(JDIRedefiner.class.getName());
+    
+    private static final String PORT_ARGUMENT_NAME = "port";
+    private static final String TRANSPORT_NAME = "dt_socket";
+
+    private VirtualMachine vm;
+
+    /** Port at which to connect to the agent of the VM. **/
+    public static final int PORT = 8787;
+    
+    public void redefineClass(Path pathToClass) {
+        
+        String className = pathToClass.getName(pathToClass.getNameCount() - 1).toString();
+        className = className.replace(".class","");
+        this.logger.log(Level.INFO, String.format("Reload class[%s]", className));
+        
+        Map<ReferenceType,byte[]> map = new HashMap<>();
+        
+        for (ReferenceType ref : vm.allClasses()){
+         
+            if (ref.name().endsWith(className)) {                        
+                
+                try {
+                    map.put(ref, IOUtils.toByteArray(
+                            new FileInputStream(pathToClass.toFile())
+                    ));                    
+                } catch (IOException ex) {
+                    Logger.getLogger(JDIRedefiner.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+            
+        }
+        
+        this.redefineClasses(map);
+        
+    }
+
+//    public Map<ReferenceType,byte[]> createMapOfReferenceTypesToBeRedefined_TEST() {
+//        
+//        Map<ReferenceType,byte[]> map = new HashMap<>();
+//        
+//        for (ReferenceType ref : vm.allClasses()){
+//            if (ref.name().contains("com.bbppbiz.controller")) {
+//                System.out.println(ref.name());
+//            }
+//            if (ref.name().equals("com.bbppbiz.controller.AuthenticationController")) {                        
+//                
+//                try {
+//                    map.put(ref, IOUtils.toByteArray(
+//                            new FileInputStream("/home/vlaugier/CODE/BBPPBIZ/bbppbiz/src/main/webapp/WEB-INF/classes/com/bbppbiz/controller/AuthenticationController.class")
+//                    ));                    
+//                } catch (IOException ex) {
+//                    Logger.getLogger(JDIRedefiner.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//
+//            }
+//            
+//        }
+//        
+//        return map;
+//        
+//    }
+    
+    public JDIRedefiner(int port) throws IOException {
+            vm = connect(port);
+    }
+
+    public void close() throws IOException {
+            disconnect();
+    }
+
+    private VirtualMachine connect(int port) throws IOException {
+            VirtualMachineManager manager = Bootstrap.virtualMachineManager();
+
+            // Find appropiate connector
+            List<AttachingConnector> connectors = manager.attachingConnectors();
+            AttachingConnector chosenConnector = null;
+            for (AttachingConnector c : connectors) {
+                    if (c.transport().name().equals(TRANSPORT_NAME)) {
+                            chosenConnector = c;
+                            break;
+                    }
+            }
+            if (chosenConnector == null) {
+                    throw new IllegalStateException("Could not find socket connector");
+            }
+
+            // Set port argument
+            AttachingConnector connector = chosenConnector;
+            Map<String, Argument> defaults = connector.defaultArguments();
+            Argument arg = defaults.get(PORT_ARGUMENT_NAME);
+            if (arg == null) {
+                    throw new IllegalStateException("Could not find port argument");
+            }
+            arg.setValue(Integer.toString(port));
+
+            // Attach
+            try {
+                    System.out.println("Connector arguments: " + defaults);
+                    return connector.attach(defaults);
+            } catch (IllegalConnectorArgumentsException e) {
+                    throw new IllegalArgumentException("Illegal connector arguments", e);
+            }
+    }
+
+    public void disconnect() {
+            if (vm != null) {
+                    vm.dispose();
+                    vm = null;
+            }
+    }
+
+    public void redefineClasses(Map<ReferenceType,byte[]> map) {             
+        for (ReferenceType refType : map.keySet()) {
+            this.logger.log(Level.INFO, "Redefine class " + refType.name());
+        }            
+        vm.redefineClasses(map);
+    }
+
+    /**
+     * Call this method before calling allClasses() in order to refresh the JDI state of loaded classes. This is
+     * necessary because the JDI map of all loaded classes is only updated based on events received over JDWP (network
+     * connection) and therefore it is not necessarily up-to-date with the real state within the VM.
+     */
+    private void refreshAllClasses() {
+            try {
+                    Field f = vm.getClass().getDeclaredField("retrievedAllTypes");
+                    f.setAccessible(true);
+                    f.set(vm, false);
+            } catch (IllegalArgumentException ex) {
+//                    Logger.getLogger(HotSwapTool.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+//                    Logger.getLogger(HotSwapTool.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NoSuchFieldException ex) {
+//                    Logger.getLogger(HotSwapTool.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SecurityException ex) {
+//                    Logger.getLogger(HotSwapTool.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+
+    private static ReferenceType findReference(List<ReferenceType> list, String name) {
+            for (ReferenceType ref : list) {
+                    if (ref.name().equals(name)) {
+                            return ref;
+                    }
+            }
+            throw new IllegalArgumentException("Cannot find corresponding reference for class name '" + name + "'");
+    }
+    
+}
